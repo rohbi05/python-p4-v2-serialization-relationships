ce2d1fe46714b5d474bbfa1cca83d82f6c660da0
shantalrohbi <shantal.rohbi@student.moringaschool.com>
2024-10-12 15:29:17 +0300

    complete lab

--- server/models.py
+++ server/models.py
@@ -9,7 +9,7 @@ convention = {
     "uq": "uq_%(table_name)s_%(column_0_name)s",
     "ck": "ck_%(table_name)s_%(constraint_name)s",
     "fk": "fk_%(table_name)s_%(column_0_name)s_%(referred_table_name)s",
-    "pk": "pk_%(table_name)s"
+    "pk": "pk_%(table_name)s",
 }
 
 metadata = MetaData(naming_convention=convention)
@@ -17,38 +17,43 @@ metadata = MetaData(naming_convention=convention)
 db = SQLAlchemy(metadata=metadata)
 
 
-class Zookeeper(db.Model):
-    __tablename__ = 'zookeepers'
-
+class Zookeeper(db.Model, SerializerMixin):
+    __tablename__ = "zookeepers"
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String, unique=True)
     birthday = db.Column(db.Date)
 
-    animals = db.relationship('Animal', back_populates='zookeeper')
+    animals = db.relationship("Animal", back_populates="zookeeper")
+    serialize_rules = ("-animals.zookeeper",)
 
 
-class Enclosure(db.Model):
-    __tablename__ = 'enclosures'
+class Enclosure(db.Model, SerializerMixin):
+    __tablename__ = "enclosures"
 
     id = db.Column(db.Integer, primary_key=True)
     environment = db.Column(db.String)
     open_to_visitors = db.Column(db.Boolean)
 
-    animals = db.relationship('Animal', back_populates='enclosure')
+    animals = db.relationship("Animal", back_populates="enclosure")
+    serialize_rules = ("-animals.enclosure",)
 
 
-class Animal(db.Model):
-    __tablename__ = 'animals'
+class Animal(db.Model, SerializerMixin):
+    __tablename__ = "animals"
 
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String, unique=True)
     species = db.Column(db.String)
 
-    zookeeper_id = db.Column(db.Integer, db.ForeignKey('zookeepers.id'))
-    enclosure_id = db.Column(db.Integer, db.ForeignKey('enclosures.id'))
+    zookeeper_id = db.Column(db.Integer, db.ForeignKey("zookeepers.id"))
+    enclosure_id = db.Column(db.Integer, db.ForeignKey("enclosures.id"))
 
-    enclosure = db.relationship('Enclosure', back_populates='animals')
-    zookeeper = db.relationship('Zookeeper', back_populates='animals')
+    enclosure = db.relationship("Enclosure", back_populates="animals")
+    zookeeper = db.relationship("Zookeeper", back_populates="animals")
+    serialize_rules = (
+        "-zookeeper.animals",
+        "-enclosure.animals",
+    )
 
     def __repr__(self):
-        return f'<Animal {self.name}, a {self.species}>'
+        return f"<Animal {self.name}, a {self.species}>"
